f6ac560fe125ae24f0c3dc5219d96c01ac302753
pitou77 <71866126+pitou77@users.noreply.github.com>
2020-09-26 20:50:42 +0700

    Update and rename ini_baru_mild.js to memek.js

--- memek.js
+++ memek.js
@@ -48,6 +48,7 @@ $(document).ready(function(){
   $('a[href^="https://mediafire.com/"]').each(function(){
     	var LDL = $(this).attr("href");
     	$(this).attr("id", "LDL"); 
+	$(this).attr("style", "display:none;"); 
     	var tautan = encodeURIComponent(LDL);
 	document.getElementById("downloadbtn").innerHTML = '<a href="https://semawur.com/full/?api=c9b69dd80965197f5477407331e1df8f870372e2&url='+tautan+'"><img alt="Download" src="https://1.bp.blogspot.com/--P4-hAsQK9U/U1W9v0oQ0yI/AAAAAAAABfU/uGg5Lffp_Es/w104/Download.png"/></a>';
 	 });
